aeaaf00811c41efad66a377bea0d4857bf20d4de
Adam VanIwaarden <avaniwaarden@gmail.com>
2013-09-05 10:25:52 -0500

    Initial Add/update SGP_CONFIG data for 2013 based on 2012 progressions

---
+++ SGP_CONFIG/EOCT/2013/ELA.R
@@ -0,0 +1,107 @@
+######################################################################
+###
+### Scripts associated with 2013 EOCT ELA: GRADE_9_LIT & AMERICAN_LIT
+###
+######################################################################
+
+### GRADE_9_LIT
+
+GRADE_9_LIT_2013.config <- list(
+	GRADE_9_LIT.2013 = list( # 1
+		sgp.content.areas=c('ELA', 'READING', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2011', '2011', '2013'),
+		sgp.grade.sequences=list(c(8,8, 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=5),
+	GRADE_9_LIT.2013 = list( # 2
+		sgp.content.areas=c('ELA', 'READING', 'ELA', 'READING', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2010', '2010', '2011', '2011', '2013'),
+		sgp.grade.sequences=list(c(7,7,8,8, 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=4),
+	GRADE_9_LIT.2013 = list( # 3
+		sgp.content.areas=c('ELA', 'READING', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2012', '2012', '2013'),
+		sgp.grade.sequences=list(c(8,8, 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=3),
+	GRADE_9_LIT.2013 = list( # 4
+		sgp.content.areas=c('ELA', 'READING', 'ELA', 'READING', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2011', '2011', '2012', '2012', '2013'),
+		sgp.grade.sequences=list(c(7,7,8,8, 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=2),
+	GRADE_9_LIT.2013 = list( # 5
+		sgp.content.areas=c('GRADE_9_LIT', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=1),
+	GRADE_9_LIT.2013 = list( # 6
+		sgp.content.areas=c('GRADE_9_LIT', 'GRADE_9_LIT'),
+		sgp.panel.years=c('2013', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=0)
+) ### END GRADE_9_LIT_2013.config
+
+
+### AMERICAN_LIT
+
+AMERICAN_LIT_2013.config <- list(
+	AMERICAN_LIT.2013 = list( # 7
+		sgp.content.areas=c('GRADE_9_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2011', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=6),
+	AMERICAN_LIT.2013 = list( # 8
+		sgp.content.areas=c('ELA', 'READING', 'GRADE_9_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2010', '2010', '2011', '2013'),
+		sgp.grade.sequences=list(c('8', '8', 'EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=5),
+	AMERICAN_LIT.2013 = list( # 9
+		sgp.content.areas=c('GRADE_9_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=4),
+	AMERICAN_LIT.2013 = list( # 10
+		sgp.content.areas=c('ELA', 'READING', 'GRADE_9_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2011', '2011', '2012', '2013'),
+		sgp.grade.sequences=list(c('8', '8', 'EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=3),
+	AMERICAN_LIT.2013 = list( # 11
+		sgp.content.areas=c('AMERICAN_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=2),
+	AMERICAN_LIT.2013 = list( # 12
+		sgp.content.areas=c('AMERICAN_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2013', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=1),
+	AMERICAN_LIT.2013 = list( # 13
+		sgp.content.areas=c('GRADE_9_LIT', 'AMERICAN_LIT'),
+		sgp.panel.years=c('2013', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=0)
+) ### END AMERICAN_LIT.2013.config
---
+++ SGP_CONFIG/EOCT/2013/SCIENCE.R
@@ -0,0 +1,191 @@
+#############################################################################
+###
+### Scripts associated with 2013 EOCT SCIENCE: Physical Science and Biology
+###
+#############################################################################
+
+### PHYSICAL_SCIENCE
+
+PHYSICAL_SCIENCE_2013.config <- list(
+	PHYSICAL_SCIENCE.2013 = list( # 40
+		sgp.content.areas=c('SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2011','2013'),
+		sgp.grade.sequences=list(c('8','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=12),
+	PHYSICAL_SCIENCE.2013 = list( # 41
+		sgp.content.areas=c('SCIENCE','SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2010','2011','2013'),
+		sgp.grade.sequences=list(c('7','8','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=11),
+	PHYSICAL_SCIENCE.2013 = list( # 42
+		sgp.content.areas=c('SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2012','2013'),
+		sgp.grade.sequences=list(c('8','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=10),
+	PHYSICAL_SCIENCE.2013 = list( # 43
+		sgp.content.areas=c('SCIENCE','SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2011','2012','2013'),
+		sgp.grade.sequences=list(c('7','8','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=9),
+	PHYSICAL_SCIENCE.2013 = list( # 44
+		sgp.content.areas=c('SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2012','2013'),
+		sgp.grade.sequences=list(c('7','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=8),
+	PHYSICAL_SCIENCE.2013 = list( # 45
+		sgp.content.areas=c('SCIENCE','SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2011','2012','2013'),
+		sgp.grade.sequences=list(c('6','7','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=7),
+	PHYSICAL_SCIENCE.2013 = list( # 46
+		sgp.content.areas=c('BIOLOGY','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2011','2013'),
+		sgp.grade.sequences=list(c('EOCT','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=6),
+	PHYSICAL_SCIENCE.2013 = list( # 47
+		sgp.content.areas=c('SCIENCE','BIOLOGY','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2010','2011','2013'),
+		sgp.grade.sequences=list(c('8','EOCT','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=5),
+	PHYSICAL_SCIENCE.2013 = list( # 48
+		sgp.content.areas=c('BIOLOGY','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2012','2013'),
+		sgp.grade.sequences=list(c('EOCT','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=4),
+	PHYSICAL_SCIENCE.2013 = list( # 49
+		sgp.content.areas=c('SCIENCE','BIOLOGY','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2011','2012','2013'),
+		sgp.grade.sequences=list(c('8','EOCT','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=3),
+	PHYSICAL_SCIENCE.2013 = list( # 50
+		sgp.content.areas=c('PHYSICAL_SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2012','2013'),
+		sgp.grade.sequences=list(c('EOCT','EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=2),
+	PHYSICAL_SCIENCE.2013 = list( # 51
+		sgp.content.areas=c('PHYSICAL_SCIENCE','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2013','2013'),
+		sgp.grade.sequences=list(c('EOCT','EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', c('LAST_OBSERVATION')),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=1),
+	PHYSICAL_SCIENCE.2013 = list( # 52
+		sgp.content.areas=c('BIOLOGY','PHYSICAL_SCIENCE'),
+		sgp.panel.years=c('2013','2013'),
+		sgp.grade.sequences=list(c('EOCT','EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=0)
+) ### END PHYSICAL_SCIENCE_2013.config
+
+
+### BIOLOGY
+
+BIOLOGY_2013.config <- list(
+	BIOLOGY.2013 = list( # 53
+		sgp.content.areas=c('SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2011', '2013'),
+		sgp.grade.sequences=list(c('8', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=11),
+	BIOLOGY.2013 = list( # 54
+		sgp.content.areas=c('SCIENCE', 'SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2010', '2011', '2013'),
+		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=10),
+	BIOLOGY.2013 = list( # 55
+		sgp.content.areas=c('SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('8', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=9),
+	BIOLOGY.2013 = list( # 56
+		sgp.content.areas=c('SCIENCE', 'SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2011', '2012', '2013'),
+		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=8),
+	BIOLOGY.2013 = list( # 57
+		sgp.content.areas=c('PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2011', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=7),
+	BIOLOGY.2013 = list( # 58
+		sgp.content.areas=c('SCIENCE', 'PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2010', '2011', '2013'),
+		sgp.grade.sequences=list(c('8', 'EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=6),
+	BIOLOGY.2013 = list( # 59
+		sgp.content.areas=c('PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=3), ### NOTE: Original sgp.norm.group.preference=5, set to 3 to make results reproducible for 2013
+	BIOLOGY.2013 = list( # 60
+		sgp.content.areas=c('SCIENCE', 'PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2011', '2012', '2013'),
+		sgp.grade.sequences=list(c('8', 'EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=5), ### NOTE: Original sgp.norm.group.preference=4, set to 5 to make results reproducible for 2013
+	BIOLOGY.2013 = list( # 61
+		sgp.content.areas=c('SCIENCE', 'PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2011', '2012', '2013'),
+		sgp.grade.sequences=list(c('7', 'EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=4), ### NOTE: Original sgp.norm.group.preference=3, set to 4 to make results reproducible for 2013
+	BIOLOGY.2013 = list( # 62
+		sgp.content.areas=c('BIOLOGY', 'BIOLOGY'),
+		sgp.panel.years=c('2012', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=2),
+	BIOLOGY.2013 = list( # 63
+		sgp.content.areas=c('BIOLOGY', 'BIOLOGY'),
+		sgp.panel.years=c('2013', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=1),
+	BIOLOGY.2013 = list( # 64
+		sgp.content.areas=c('PHYSICAL_SCIENCE', 'BIOLOGY'),
+		sgp.panel.years=c('2013', '2013'),
+		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
+		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.exact.grade.progression=TRUE,
+		sgp.norm.group.preference=0)
+) ### END BIOLOGY_2013.config
