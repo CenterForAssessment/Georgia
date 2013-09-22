d9c5bc320feef4fc9e3d141a8ed0d827baf7d2a4
dbetebenner <dbetebenner@gmail.com>
2013-09-22 06:00:18 -0400

    Removed old configs in 2013 configurations

--- SGP_CONFIG/EOCT/2013/MATHEMATICS.R
+++ SGP_CONFIG/EOCT/2013/MATHEMATICS.R
@@ -4,92 +4,9 @@
 ###
 ###########################################################################################################
 
-### Algebra
-
-#ALGEBRA_2013.config <- list(
-#	ALGEBRA.2013 = list( # 14
-#		sgp.content.areas=c('MATHEMATICS', 'ALGEBRA'),
-#		sgp.panel.years=c('2011', '2013'),
-#		sgp.grade.sequences=list(c('8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=4),
-#	ALGEBRA.2013 = list( # 15
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA'),
-#		sgp.panel.years=c('2010', '2011', '2013'),
-#		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=3),
-#	ALGEBRA.2013 = list( # 16
-#		sgp.content.areas=c('MATHEMATICS', 'ALGEBRA'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=2),
-#	ALGEBRA.2013 = list( # 17
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'ALGEBRA'),
-#		sgp.panel.years=c('2011', '2012', '2013'),
-#		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=1),
-#	ALGEBRA.2013 = list( # 18
-#		sgp.content.areas=c('ALGEBRA', 'ALGEBRA'),
-#		sgp.panel.years=c('2013', '2013'),
-#		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=0)
-#) ### END ALGEBRA_2013.config
-
-
 ### Geometry
 
 GEOMETRY_2013.config <- list(
-#	GEOMETRY.2013 = list( # 19
-#		sgp.content.areas=c('MATHEMATICS', 'GEOMETRY'),
-#		sgp.panel.years=c('2011', '2013'),
-#		sgp.grade.sequences=list(c('8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=5),
-#	GEOMETRY.2013 = list( # 20
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY'),
-#		sgp.panel.years=c('2010', '2011', '2013'),
-#		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=4),
-#	GEOMETRY.2013 = list( # 21
-#		sgp.content.areas=c('MATHEMATICS', 'GEOMETRY'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=3),
-#	GEOMETRY.2013 = list( # 22
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'GEOMETRY'),
-#		sgp.panel.years=c('2011', '2012', '2013'),
-#		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=2),
-#	GEOMETRY.2013 = list( # 23
-#		sgp.content.areas=c('MATHEMATICS_I', 'GEOMETRY'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=1),
-#	GEOMETRY.2013 = list( # 24
-#		sgp.content.areas=c('GEOMETRY', 'GEOMETRY'),
-#		sgp.panel.years=c('2013', '2013'),
-#		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=0)
 	GEOMETRY.2013 = list( # 20
 		sgp.content.areas=c('ALGEBRA_I', 'GEOMETRY'),
 		sgp.panel.years=c('2012', '2013'),
@@ -179,48 +96,6 @@ MATHEMATICS_I_2013.config <- list(
 		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
 		sgp.exact.grade.progression=TRUE,
 		sgp.norm.group.preference=1)
-#	MATHEMATICS_I.2013 = list( # 27
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=5),
-#	MATHEMATICS_I.2013 = list( # 28
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2011', '2012', '2013'),
-#		sgp.grade.sequences=list(c('7', '8', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=4),
-#	MATHEMATICS_I.2013 = list( # 29
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('7', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=3),
-#	MATHEMATICS_I.2013 = list( # 30
-#		sgp.content.areas=c('MATHEMATICS', 'MATHEMATICS', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2011', '2012', '2013'),
-#		sgp.grade.sequences=list(c('6', '7', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=2),
-#	MATHEMATICS_I.2013 = list( # 31
-#		sgp.content.areas=c('MATHEMATICS_I', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2012', '2013'),
-#		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=1),
-#	MATHEMATICS_I.2013 = list( # 32
-#		sgp.content.areas=c('MATHEMATICS_I', 'MATHEMATICS_I'),
-#		sgp.panel.years=c('2013', '2013'),
-#		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-#		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
-#		sgp.exact.grade.progression=TRUE,
-#		sgp.norm.group.preference=0)
 ) ### END MATHEMATICS_I_2013.config
 
 
