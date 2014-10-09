7a4cad5027b6a10980ff89cb8a29db1b0c666a5b
Adam VanIwaarden <avaniwaarden@gmail.com>
2014-10-09 11:42:26 -0600

    Fix SCHOOL_NUMBER in 2014 LONG data prep/cleaning

--- Georgia_Data_LONG_2014_STEP_1b.R
+++ Georgia_Data_LONG_2014_STEP_1b.R
@@ -18,9 +18,10 @@ Georgia_Data_LONG_2014 <- fread("Data/Base_Files/Georgia_Data_LONG_2014_Step_1a.
 
 Georgia_Data_LONG_2014[,SCHOOL_YEAR:=as.character(SCHOOL_YEAR)]
 
+Georgia_Data_LONG_2014[, SR_SYSTEM_ID:=as.integer(SR_SYSTEM_ID)]
+Georgia_Data_LONG_2014[, SCHOOL_NUMBER:=as.integer(SCHOOL_NUMBER)]
 Georgia_Data_LONG_2014$SCHOOL_NUMBER <- Georgia_Data_LONG_2014$SR_SYSTEM_ID*10000 + Georgia_Data_LONG_2014$SCHOOL_NUMBER
 Georgia_Data_LONG_2014[which(SR_SYSTEM_ID >1000), SCHOOL_NUMBER:=SR_SYSTEM_ID]
-Georgia_Data_LONG_2014[, SCHOOL_NUMBER:=as.integer(SCHOOL_NUMBER)]
 
 Georgia_Data_LONG_2014[, FIRST_NAME:=as.factor(FIRST_NAME)]
 Georgia_Data_LONG_2014[, MIDDLE_NAME:=as.factor(MIDDLE_NAME)]
@@ -57,6 +58,9 @@ levels(Georgia_Data_LONG_2014$SWD)[1] <- NA
 Georgia_Data_LONG_2014[,LEP:=as.factor(LEP)]
 levels(Georgia_Data_LONG_2014$LEP)[1] <- NA
 
+Georgia_Data_LONG_2014[,GIFT:=as.factor(GIFT)]
+levels(Georgia_Data_LONG_2014$GIFT)[1] <- NA
+
 Georgia_Data_LONG_2014[,PERFORMANCE_LEVEL:=factor(PERFORMANCE_LEVEL, levels=c("Does Not Meet Expectations", "Meets Expectations", "Exceeds Expectations"), ordered=TRUE)]
 
 Georgia_Data_LONG_2014[,SCHOOL_ENROLLMENT_STATUS:=factor(SCHOOL_ENROLLMENT_STATUS, levels=c("Enrolled School: No", "Enrolled School: Yes"), labels=c("Enrolled School: No", "Enrolled School: Yes"))]
@@ -65,11 +69,26 @@ Georgia_Data_LONG_2014[,DISTRICT_ENROLLMENT_STATUS:=factor(DISTRICT_ENROLLMENT_S
 
 Georgia_Data_LONG_2014[,STATE_ENROLLMENT_STATUS:=factor(STATE_ENROLLMENT_STATUS, levels=c("Enrolled State: No", "Enrolled State: Yes"), labels=c("Enrolled State: No", "Enrolled State: Yes"))]
 
-Georgia_Data_LONG_2014[,GIFT:=as.factor(GIFT)]
-levels(Georgia_Data_LONG_2014$GIFT)[1] <- NA
-
 ### Save results
 
 save(Georgia_Data_LONG_2014, file="Data/Georgia_Data_LONG_2014.Rdata")
 
+###############################################################################################
+###
+###   Update the SGP object with the 2014 data
+###   Manual update of @Data - much quicker than using updateSGP with prepareSGP step
+###
+###############################################################################################
+
+load('../Georgia_SGP.Rdata')
+
+require(plyr)
+
+GA_2014 <- prepareSGP(Georgia_Data_LONG_2014)
+sgp.key <- key(Georgia_SGP@Data)
+Georgia_SGP@Data <- data.table(rbind.fill(Georgia_SGP@Data, GA_2014@Data), key=sgp.key)
+
+Georgia_SGP <- prepareSGP(Georgia_SGP)
+
+save(Georgia_SGP, file="Data/Georgia_SGP.Rdata")
 
