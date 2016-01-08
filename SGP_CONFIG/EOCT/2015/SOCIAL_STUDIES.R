2def0f14403d657d124984f831faf15bc7be2c8d
Adam VanIwaarden <avaniwaarden@gmail.com>
2016-01-08 13:06:42 -0700

    Switch first/last obs for block schedule

--- SGP_CONFIG/EOCT/2015/SOCIAL_STUDIES.R
+++ SGP_CONFIG/EOCT/2015/SOCIAL_STUDIES.R
@@ -139,7 +139,7 @@ ECONOMICS_2015.config <- list(
 		sgp.content.areas=c('US_HISTORY', 'ECONOMICS'),
 		sgp.panel.years=c('2015', '2015'),
 		sgp.grade.sequences=list(c('EOCT', 'EOCT')),
-		sgp.panel.years.within=c('FIRST_OBSERVATION', 'LAST_OBSERVATION'),
+		sgp.panel.years.within=c('LAST_OBSERVATION', 'FIRST_OBSERVATION'),
 		sgp.exact.grade.progression=TRUE,
 		sgp.norm.group.preference=0)
 ) ### END ECONOMICS_2015.config
