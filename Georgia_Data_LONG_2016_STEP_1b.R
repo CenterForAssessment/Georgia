bb1ed0b986d0ef5271ac91d5dc9474a4c619a23c
dbetebenner <dbetebenner@gmail.com>
2016-05-26 11:03:02 -0400

    Adding in first 2016 data prep script

--- Georgia_Data_LONG_2016_STEP_1b.R
+++ Georgia_Data_LONG_2016_STEP_1b.R
@@ -1,6 +1,6 @@
 ###############################################################################################
 ###
-###   Prepare and format the 2015 EOGT and EOCT Milestones data provided by GA DOE
+###   Prepare and format the 2016 EOGT and EOCT Milestones data provided by GA DOE
 ###
 ###############################################################################################
 
@@ -9,86 +9,86 @@ require(data.table)
 setwd('~/SGP_Projects/Georgia')
 
 ###
-###		Load 2015 Raw Data
+###		Load 2016 Raw Data
 ###
 
-Georgia_Data_LONG_2014 <- as.data.table(read.delim(unz('Data/Base_Files/2014_EOCT_TestOut_Data.zip', 
-							'2014_EOCT_TestOut_Data.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
+Georgia_Data_LONG_2015 <- as.data.table(read.delim(unz('Data/Base_Files/2015_EOCT_TestOut_Data.zip', 
+							'2015_EOCT_TestOut_Data.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
 
-Georgia_Data_LONG_2015 <- as.data.table(read.delim(unz('Data/Base_Files/2015_Georgia_Milestones_All_Data.zip', 
-							'2015_Georgia_Milestones_All_Data.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
-Georgia_Data_LONG_2015[, RESCORE := 0]
+Georgia_Data_LONG_2016 <- as.data.table(read.delim(unz('Data/Base_Files/2016_Georgia_Milestones_All_Data.zip', 
+							'2016_Georgia_Milestones_All_Data.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
+Georgia_Data_LONG_2016[, RESCORE := 0]
 
-Georgia_2015_Rescores <- as.data.table(read.delim(unz('Data/Base_Files/2015_Georgia_Milestones_rescore.zip', 
-							'2015_Georgia_Milestones_rescore.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
-Georgia_2015_Rescores[, RESCORE := 1]
+Georgia_2016_Rescores <- as.data.table(read.delim(unz('Data/Base_Files/2016_Georgia_Milestones_rescore.zip', 
+							'2016_Georgia_Milestones_rescore.txt'), sep='|', header=TRUE, stringsAsFactors=FALSE))
+Georgia_2016_Rescores[, RESCORE := 1]
 
-setnames(Georgia_Data_LONG_2014, 'ACHIVEMENT_LEVEL', 'PERFORMANCE_LEVEL')
-setnames(Georgia_Data_LONG_2015, 'CONDSEM', 'SCALE_SCORE_CSEM')
-setnames(Georgia_2015_Rescores, 'CONDSEM', 'SCALE_SCORE_CSEM')
+setnames(Georgia_Data_LONG_2015, 'ACHIVEMENT_LEVEL', 'PERFORMANCE_LEVEL')
+setnames(Georgia_Data_LONG_2016, 'CONDSEM', 'SCALE_SCORE_CSEM')
+setnames(Georgia_2016_Rescores, 'CONDSEM', 'SCALE_SCORE_CSEM')
 
 
-###  Combine 2014 Test Out CRCT and 2015 Milestones data
-Georgia_Data_LONG_2015 <- rbindlist(list(Georgia_Data_LONG_2014, Georgia_Data_LONG_2015[SUBJECT_CODE != "NULL"], Georgia_2015_Rescores), fill=TRUE)
+###  Combine 2015 Test Out CRCT and 2016 Milestones data
+Georgia_Data_LONG_2016 <- rbindlist(list(Georgia_Data_LONG_2015, Georgia_Data_LONG_2016[SUBJECT_CODE != "NULL"], Georgia_2016_Rescores), fill=TRUE)
 
 
 ### Tidy up data
 
-Georgia_Data_LONG_2015[, SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)*10000 + as.numeric(SCHOOL_NUMBER)]
-Georgia_Data_LONG_2015[which(SR_SYSTEM_ID > 1000), SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)]
-Georgia_Data_LONG_2015[, SCHOOL_NUMBER := as.integer(SCHOOL_NUMBER)]
+Georgia_Data_LONG_2016[, SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)*10000 + as.numeric(SCHOOL_NUMBER)]
+Georgia_Data_LONG_2016[which(SR_SYSTEM_ID > 1000), SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)]
+Georgia_Data_LONG_2016[, SCHOOL_NUMBER := as.integer(SCHOOL_NUMBER)]
 
-Georgia_Data_LONG_2015[, GRADE := gsub("0", "", GRADE)]
-Georgia_Data_LONG_2015[, GRADE_REPORTED := as.integer(GRADE_REPORTED)]
+Georgia_Data_LONG_2016[, GRADE := gsub("0", "", GRADE)]
+Georgia_Data_LONG_2016[, GRADE_REPORTED := as.integer(GRADE_REPORTED)]
 
-Georgia_Data_LONG_2015[, SCALE_SCORE := as.numeric(SCALE_SCORE)]
-Georgia_Data_LONG_2015[, SCALE_SCORE_CSEM := as.numeric(SCALE_SCORE_CSEM)]
+Georgia_Data_LONG_2016[, SCALE_SCORE := as.numeric(SCALE_SCORE)]
+Georgia_Data_LONG_2016[, SCALE_SCORE_CSEM := as.numeric(SCALE_SCORE_CSEM)]
 
-Georgia_Data_LONG_2015[, ADMINISTRATION_PERIOD := toupper(ADMINISTRATION_PERIOD)]
-Georgia_Data_LONG_2015[, ADMINISTRATION_PERIOD := paste(YEAR_WITHIN, ADMINISTRATION_PERIOD, sep=": ")]
-Georgia_Data_LONG_2015[which(ADMINISTRATION_PERIOD == "1: "), ADMINISTRATION_PERIOD := "1: WINTER"]
-Georgia_Data_LONG_2015[which(ADMINISTRATION_PERIOD == "2: "), ADMINISTRATION_PERIOD := "2: SPRING"]
-Georgia_Data_LONG_2015[which(ADMINISTRATION_PERIOD == "3: "), ADMINISTRATION_PERIOD := "3: SUMMER"]
+Georgia_Data_LONG_2016[, ADMINISTRATION_PERIOD := toupper(ADMINISTRATION_PERIOD)]
+Georgia_Data_LONG_2016[, ADMINISTRATION_PERIOD := paste(YEAR_WITHIN, ADMINISTRATION_PERIOD, sep=": ")]
+Georgia_Data_LONG_2016[which(ADMINISTRATION_PERIOD == "1: "), ADMINISTRATION_PERIOD := "1: WINTER"]
+Georgia_Data_LONG_2016[which(ADMINISTRATION_PERIOD == "2: "), ADMINISTRATION_PERIOD := "2: SPRING"]
+Georgia_Data_LONG_2016[which(ADMINISTRATION_PERIOD == "3: "), ADMINISTRATION_PERIOD := "3: SUMMER"]
 
-Georgia_Data_LONG_2015[,SCHOOL_ENROLLMENT_STATUS := factor(1, levels=0:1, 
+Georgia_Data_LONG_2016[,SCHOOL_ENROLLMENT_STATUS := factor(1, levels=0:1, 
 	labels=c("Enrolled School: No", "Enrolled School: Yes"))]
 
-Georgia_Data_LONG_2015[,DISTRICT_ENROLLMENT_STATUS := factor(1, levels=0:1, 
+Georgia_Data_LONG_2016[,DISTRICT_ENROLLMENT_STATUS := factor(1, levels=0:1, 
    labels=c("Enrolled District: No", "Enrolled District: Yes"))]
 
-Georgia_Data_LONG_2015[,STATE_ENROLLMENT_STATUS := factor(1, levels=0:1, 
+Georgia_Data_LONG_2016[,STATE_ENROLLMENT_STATUS := factor(1, levels=0:1, 
    labels=c("Enrolled State: No", "Enrolled State: Yes"))]
 
 ###  Remove known duplicate cases:
-Georgia_Data_LONG_2015<-Georgia_Data_LONG_2015[-which(Georgia_Data_LONG_2015$GTID %in% c('3105982299','1747050385','2723026345') & Georgia_Data_LONG_2015$SCHOOL_YEAR=='2014'),]
+Georgia_Data_LONG_2016<-Georgia_Data_LONG_2016[-which(Georgia_Data_LONG_2016$GTID %in% c('3105982299','1747050385','2723026345') & Georgia_Data_LONG_2016$SCHOOL_YEAR=='2015'),]
 
 ###  Remove original rescored cases
-setkey(Georgia_Data_LONG_2015, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GRADE, GTID, RESCORE)
-setkey(Georgia_Data_LONG_2015, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GRADE, GTID)
-# sum(duplicated(Georgia_Data_LONG_2015[VALID_CASE != "INVALID_CASE"])) # 10 duplicates with valid SSIDs -- all have same SSID and esID, so appear valid - take the highest score
-# dups <- data.table(Georgia_Data_LONG_2015[unique(c(which(duplicated(Georgia_Data_LONG_2015))-1, which(duplicated(Georgia_Data_LONG_2015)))), ], key=key(Georgia_Data_LONG_2015))
-Georgia_Data_LONG_2015[which(duplicated(Georgia_Data_LONG_2015))-1, VALID_CASE := "INVALID_CASE"]
+setkey(Georgia_Data_LONG_2016, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GRADE, GTID, RESCORE)
+setkey(Georgia_Data_LONG_2016, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GRADE, GTID)
+# sum(duplicated(Georgia_Data_LONG_2016[VALID_CASE != "INVALID_CASE"])) # 10 duplicates with valid SSIDs -- all have same SSID and esID, so appear valid - take the highest score
+# dups <- data.table(Georgia_Data_LONG_2016[unique(c(which(duplicated(Georgia_Data_LONG_2016))-1, which(duplicated(Georgia_Data_LONG_2016)))), ], key=key(Georgia_Data_LONG_2016))
+Georgia_Data_LONG_2016[which(duplicated(Georgia_Data_LONG_2016))-1, VALID_CASE := "INVALID_CASE"]
 
 ### Save results
 
-save(Georgia_Data_LONG_2015, file="Data/Georgia_Data_LONG_2015.Rdata")
+save(Georgia_Data_LONG_2016, file="Data/Georgia_Data_LONG_2016.Rdata")
 
 
 ###
 ###		Create New Georgia SGP object for Milestones Assessments with Only CRCT Priors
 ###
 
-###  Use @Data from outputSGP in 2014 CRCT analyses as the base for the new object
+###  Use @Data from outputSGP in 2015 CRCT analyses as the base for the new object
 #load("Data/Georgia_SGP_LONG_Data.Rdata")
-load("Data/Archive/December_2015/Georgia_SGP_LONG_Data.Rdata")
+load("Data/Archive/December_2016/Georgia_SGP_LONG_Data.Rdata")
 
 ###  Select out only a subset of the data - Only CRCT EOGT and EOCT subjects and years that will be used as priors.
 CRCT_Subjects <- c("ELA", "READING", "SOCIAL_STUDIES", "SCIENCE", "MATHEMATICS")
 EOCT_Subjects <- c("GRADE_9_LIT", "AMERICAN_LIT", "ECONOMICS", "US_HISTORY", "BIOLOGY", "PHYSICAL_SCIENCE", "COORDINATE_ALGEBRA", "ANALYTIC_GEOMETRY")
 
 Georgia_LONG_Data <- rbindlist(list( 
-						Georgia_SGP_LONG_Data[VALID_CASE == "VALID_CASE" & SCHOOL_YEAR %in% 2012:2014 & SUBJECT_CODE %in% c(CRCT_Subjects, EOCT_Subjects)],
-						Georgia_SGP_LONG_Data[VALID_CASE == "VALID_CASE" & SCHOOL_YEAR %in% 2011 & SUBJECT_CODE == "SOCIAL_STUDIES" & GRADE == 8]))
+						Georgia_SGP_LONG_Data[VALID_CASE == "VALID_CASE" & SCHOOL_YEAR %in% 2013:2015 & SUBJECT_CODE %in% c(CRCT_Subjects, EOCT_Subjects)],
+						Georgia_SGP_LONG_Data[VALID_CASE == "VALID_CASE" & SCHOOL_YEAR %in% 2012 & SUBJECT_CODE == "SOCIAL_STUDIES" & GRADE == 8]))
 
 ###  Remove all BASELINE SGP variables and HIGH_NEED_STATUS variable (never used and adds considerable time to prepareSGP step)
 Georgia_LONG_Data[, grep("BASELINE", names(Georgia_LONG_Data)) := NULL]
