2e6abe199bc0f63aa9eed3838085bb5696e0a371
qinqi715 <qinqi715@gmail.com>
2017-07-18 11:35:40 -0400

    Georgia 2017 EOG step 1b

---
+++ Georgia_Data_LONG_2017_STEP_1b.R
@@ -0,0 +1,61 @@
+#####################################################################################
+###                                                                               ###
+###           Prepare and format the 2017 EOG and EOC Georgia Milestones data     ###
+###                                                                               ###
+#####################################################################################
+
+require(data.table)
+
+
+###
+###		Load 2017 Raw Data -  Clean EOG and EOC seperately (delivered seperately)
+###
+
+###  GADOE data loading process
+##
+#
+
+setwd('U:/DATA/SGP/Data/2017 SGPs/SGP Calculation/Working Directory_QQ/')
+
+
+
+#####Load 2017 EOG Data#####
+
+
+names(Georgia_Data_LONG_2017)
+
+Georgia_Data_LONG_2017 <- data.table(read.delim("U:/DATA/SGP/Data/2017 SGPs/Computer Matched Data/2017_Georgia_Milestones_EOG_preliminary.txt",  header = TRUE, sep = "|"))
+
+###   Tidy up data
+
+setnames(Georgia_Data_LONG_2017, 'CONDSEM', 'SCALE_SCORE_CSEM')
+
+Georgia_Data_LONG_2017[, SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)*10000 + as.numeric(SR_SCHOOL_ID)]
+Georgia_Data_LONG_2017[which(as.numeric(SR_SYSTEM_ID) > 1000), SCHOOL_NUMBER := as.numeric(SR_SYSTEM_ID)]
+Georgia_Data_LONG_2017[, SCHOOL_NUMBER := as.integer(SCHOOL_NUMBER)]
+
+Georgia_Data_LONG_2017[, SCALE_SCORE := as.numeric(SCALE_SCORE)]
+Georgia_Data_LONG_2017[, SCALE_SCORE_CSEM := as.numeric(SCALE_SCORE_CSEM)]
+
+Georgia_Data_LONG_2017[, GRADE := gsub("0", "", GRADE)]
+
+Georgia_Data_LONG_2017[, ADMINISTRATION_PERIOD := paste(YEAR_WITHIN, toupper(ADMINISTRATION_PERIOD), sep=": ")]
+
+Georgia_Data_LONG_2017[,SCHOOL_ENROLLMENT_STATUS := factor(1, levels=0:1, labels=c("Enrolled School: No", "Enrolled School: Yes"))]
+Georgia_Data_LONG_2017[,DISTRICT_ENROLLMENT_STATUS := factor(1, levels=0:1, labels=c("Enrolled District: No", "Enrolled District: Yes"))]
+Georgia_Data_LONG_2017[,STATE_ENROLLMENT_STATUS := factor(1, levels=0:1, labels=c("Enrolled State: No", "Enrolled State: Yes"))]
+
+
+###  Invalidate duplicates(No duplicate cases in EOG file)
+#setkey(Georgia_Data_LONG_2017, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GTID, SCALE_SCORE)
+#setkey(Georgia_Data_LONG_2017, VALID_CASE, SUBJECT_CODE, SCHOOL_YEAR, YEAR_WITHIN, GTID)
+# sum(duplicated(Georgia_Data_LONG_2017[VALID_CASE != "INVALID_CASE"], by=key(Georgia_Data_LONG_2017))) # 0 duplicates with valid GTIDs - take the highest score
+# dups <- data.table(Georgia_Data_LONG_2017[unique(c(which(duplicated(Georgia_Data_LONG_2017, by=key(Georgia_Data_LONG_2017)))-1, which(duplicated(Georgia_Data_LONG_2017, by=key(Georgia_Data_LONG_2017))))), ], key=key(Georgia_Data_LONG_2017))
+# Georgia_Data_LONG_2017[which(duplicated(Georgia_Data_LONG_2017, by=key(Georgia_Data_LONG_2017)))-1, VALID_CASE := "INVALID_CASE"]
+
+### Save results - Save EOG and EOC seperately (delivered seperately)
+
+Georgia_Data_LONG_2017_EOG <- Georgia_Data_LONG_2017
+save(Georgia_Data_LONG_2017_EOG, file="Georgia_Data_LONG_2017_EOG.Rdata")
+
+# save(Georgia_Data_LONG_2017, file="Data/Georgia_Data_LONG_2017.Rdata")
