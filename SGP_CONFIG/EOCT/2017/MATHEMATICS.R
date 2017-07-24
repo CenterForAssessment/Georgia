f41f643b4565ccd3fbe8bd032ec4408d456fb703
qinqi715 <qinqi715@gmail.com>
2017-07-24 09:06:43 -0400

    Update MATHEMATICS.R

--- SGP_CONFIG/EOCT/2017/MATHEMATICS.R
+++ SGP_CONFIG/EOCT/2017/MATHEMATICS.R
@@ -15,7 +15,7 @@ MATHEMATICS_2017.config <- list(
 		sgp.exact.grade.progression=list(TRUE, TRUE, TRUE, TRUE, TRUE),
 		sgp.projection.sequence = c("MATH_COORD_ALG", "MATH_ALG_I"),
 		sgp.norm.group.preference=2),
-	MATHEMATICS.2016 = list(
+	MATHEMATICS.2017 = list(
 		sgp.content.areas=rep('MATHEMATICS', 3),
 		sgp.panel.years.within=c(rep('LAST_OBSERVATION', 2), 'FIRST_OBSERVATION'),
 		sgp.panel.years=c('2015', '2016', '2017'),
